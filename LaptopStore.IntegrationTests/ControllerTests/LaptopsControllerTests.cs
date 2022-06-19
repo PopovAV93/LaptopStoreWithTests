58b2afec0350c880ae260ec21c2e6b138e2b0de6
saruwatar@gmail.com <Pwgqsyx2pwgqsyx2>
2022-06-19 16:49:55 +0500

    Controller tests added

---
+++ LaptopStore.IntegrationTests/ControllerTests/LaptopsControllerTests.cs
@@ -0,0 +1,91 @@
+﻿using LaptopStore.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using System.Net;
+using System.Net.Http;
+using LaptopStore.Data.Models;
+using LaptopStore.ViewModels;
+using Microsoft.Extensions.Configuration;
+using LaptopStore.Data.Repository;
+using LaptopStore.Data.Mocks;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using LaptopStore.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaptopStore.IntegrationTests.ControllerTests
+{
+    [TestFixture]
+    public class LaptopsControllerTests
+    {
+        private readonly TestHelper testHelper = new();
+
+        [Test]
+        public async Task List_ShouldGetLaptopListByCategory()
+        {
+            // Arrange
+
+            AppDBContent dbContext = testHelper.GetAppDBContent();
+
+            await dbContext.Database.EnsureDeletedAsync();
+            await dbContext.Database.EnsureCreatedAsync();
+
+            List<Category> mockCategories = new MockLaptopCategories().AllCategories.ToList();
+            List<Laptop> mockLaptops = new MockLaptops().getLaptops.ToList();
+
+            
+            await dbContext.Categories.AddRangeAsync(mockCategories);
+            await dbContext.Laptops.AddRangeAsync(mockLaptops);
+            await dbContext.SaveChangesAsync();
+
+            // Act
+            LaptopRepository laptopRepository = new LaptopRepository(dbContext);
+            CategoryRepository categorieRepository = new CategoryRepository(dbContext);
+            LaptopsController controller = new LaptopsController(laptopRepository, categorieRepository);
+
+            ViewResult result = controller.List(mockCategories[0].categoryName);
+
+            LaptopListViewModel resultModel = (LaptopListViewModel) result.Model;
+
+            // Assert
+
+            Assert.That(resultModel.allLaptops, Is.EqualTo(dbContext.Laptops.Where(x => x.categoryId == mockCategories[0].id)));
+        }
+
+        [Test]
+        public async Task LaptopDetails_ShouldGetLaptopDetailsByLaptopId()
+        {
+            // Arrange
+
+            AppDBContent dbContext = testHelper.GetAppDBContent();
+
+            await dbContext.Database.EnsureDeletedAsync();
+            await dbContext.Database.EnsureCreatedAsync();
+
+            List<Laptop> mockLaptops = new MockLaptops().getLaptops.ToList();
+
+
+            await dbContext.Laptops.AddRangeAsync(mockLaptops);
+            await dbContext.SaveChangesAsync();
+
+            // Act
+            LaptopRepository laptopRepository = new LaptopRepository(dbContext);
+            CategoryRepository categorieRepository = new CategoryRepository(dbContext);
+            LaptopsController controller = new LaptopsController(laptopRepository, categorieRepository);
+
+            ViewResult result = controller.LaptopDetails(mockLaptops[0].id);
+
+            // Assert
+
+            Assert.That(mockLaptops[0], Is.EqualTo(result.Model));
+        }
+    }
+}
---
+++ LaptopStore.IntegrationTests/ControllerTests/OrderControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using LaptopStore.Data;
+using NUnit.Framework;
+using System.Linq;
+using System.Threading.Tasks;
+using LaptopStore.Data.Models;
+using LaptopStore.Data.Repository;
+using LaptopStore.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaptopStore.IntegrationTests.ControllerTests
+{
+    [TestFixture]
+    public class OrderControllerTests
+    {
+        private readonly TestHelper testHelper = new();
+
+        [Test]
+        public async Task Checkout_ShouldReturnModelStateError()
+        {
+            //Arrange
+
+            AppDBContent dbContext = testHelper.GetAppDBContent();
+
+            await dbContext.Database.EnsureDeletedAsync();
+            await dbContext.Database.EnsureCreatedAsync();
+
+            Cart cart = new Cart(dbContext);
+
+            Order order = new Order() { id = 1, userId = 1, address = "test1", firstName = "test1", lastName = "test1", phoneNumber = "123-456-7890" };
+            OrderController controller = new OrderController(new OrderRepository(dbContext, cart), new UserRepository(dbContext), cart);
+
+            //Act
+
+            ViewResult result = (ViewResult) await controller.Checkout(order);
+
+            //Assert
+
+            Assert.That(result.ViewData.ModelState.Values.FirstOrDefault().Errors.FirstOrDefault().ErrorMessage, Is.EqualTo("Your shopping cart must contain items"));
+
+        }
+    }
+}
